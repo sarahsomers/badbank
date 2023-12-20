9fef271590088a45a8f97e7721d7c5a02335371e
sarahsomers <122656157+sarahsomers@users.noreply.github.com>
2023-12-20 14:12:34 -0800

    Update home.js

--- home.js
+++ home.js
@@ -2,12 +2,10 @@ function Home(){
   return (
     <Card
       txtcolor="black"
-      header="A Bad Bank"
-      title="Welcome to the bank"
+      header={(<img src="bankheader.png" className="img-fluid" alt="Responsive image"/>)}
+      title="Nevada's baddest bank!"
       text="You can move around using the navigation bar."
-      body={(<img src="bank.png" className="img-fluid" alt="Responsive image"/>)}
+      body={(<img src="banknev.png" className="img-fluid" alt="Responsive image"/>)}
     />
   );  
 }
-
-
